821615a14e97dfdf0a574d66da96303bbd94e8ca
FloatCOIN <floatbeta@gmail.com>
2020-05-26 01:18:25 -0500

    Add files via upload

---
+++ src/rightpeer/rightpeer.h
@@ -0,0 +1,23 @@
+// Copyright (c) RightPeer Limited 
+// RightPeer code distributed under the GPLv3 license, see COPYING file.
+
+#ifndef RIGHTPEER_H
+#define	RIGHTPEER_H
+
+
+#include "utils/define.h"
+#include "utils/declare.h"
+    
+#include "chainparams/chainparams.h"
+#include "protocol/rightpeerscript.h"
+#include "permissions/permission.h"
+#include "entities/asset.h"
+#include "chainparams/state.h"
+
+    
+extern unsigned int MIN_RELAY_TX_FEE;  
+extern unsigned int MIN_OFFCHAIN_FEE;  
+
+
+#endif	/* RIGHTPEER_H */
+
